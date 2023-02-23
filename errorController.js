beab24be8ca0e5ecc4220b6369dd1bfad65f5ada
tyrese-s <tyrese.sylvester@yahoo.com>
2023-02-23 13:24:28 +0000

    completed #7

--- errorController.js
+++ errorController.js
@@ -8,6 +8,10 @@ exports.handleCustomErrors = (err, req, res, next) => {
 exports.psqlErrors = (err, req, res, next) => {
     if(err.code === '22P02'){
         res.status(400).send({msg: 'invalid input id'})
+    }else if (err.code === '23502'){
+        res.status(400).send({msg: 'missing/incorrect fields!'})
+    } else if (err.code === '42804'){
+        res.status(400).send({msg: 'input id is not a number'})
     }
     else next(err)
 }
