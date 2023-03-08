bd1c3e98465ab0e693240dbb41bfcdbc7731e475
tyrese-s <tyrese.sylvester@yahoo.com>
2023-03-08 13:48:11 +0000

    rearranged connection file

--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,13 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
-// require('dotenv').config({
-//   path: `${__dirname}/../.env.${ENV}`,
-// });
+require('dotenv').config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
 
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
+}
 const config =
   ENV === 'production'
     ? {
@@ -13,8 +16,5 @@ const config =
       }
     : {};
 
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE or DATABASE_URL not set');
-}
 
 module.exports = new Pool(config);
